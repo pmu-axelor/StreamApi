9838909799120d1625ddaf118ac737498e8c2b85
Parvej Muhamad <pmu.axelor@gmail.com>
2021-08-26 17:39:05 +0530

    initial commit

---
+++ src/main/java/PeekMethod.java
@@ -0,0 +1,11 @@
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class PeekMethod {
+   private void psvm() {
+	// TODO Auto-generated method stub
+
+	example1();
+}
+ 
+}
